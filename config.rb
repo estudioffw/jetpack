2c0581840512904a1d32d631622148950a8b5fbe
perdidao <lucas@perdidao.net>
2014-07-26 19:14:08 -0300

    Primeiro commit

---
+++ config.rb
@@ -0,0 +1,26 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/css"
+sass_dir = "dynamic/scss"
+images_dir = "static/img"
+javascripts_dir = "dynamic/js"
+fonts_dir = "static/font"
+
+output_style = :compact
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass dynamic/scss scss && rm -rf sass && mv scss sass
